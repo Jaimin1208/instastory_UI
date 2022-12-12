9e10debfe6174fcd40c9e79b8009b1db9fb649d2
jaimin1208 <jaiminpanchal1208@gmail.com>
2022-12-12 14:34:12 -0500

    sdfvsdf

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      columns: {
+        "1xl": "40rem"
+      }
+    },
+  },
+  plugins: [],
+}
